d9a31b0ad67f88f4a2b69159084ab9a3e16f8427
adityar22 <adityaramadhanarckarito47@gmail.com>
2023-03-22 14:50:54 +0700

    update backend

--- backend/server/router/plan.js
+++ backend/server/router/plan.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const route = express.Router();
+const planController = require('../controller/plan')
 
 //plan router
+route.post('/', planController.createPlan)
+route.get('/', planController.getAll)
+route.get('/:id', planController.getByUserID)
+route.put('/:id', planController.edit)
 
 
 module.exports= route
