4e62339e6b84f37f3e4d774fa532eac174131623
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-03-30 14:40:06 +0700

    feat: add carousel component

---
+++ backend/frontend/src/components/carousel/recommenData.js
@@ -0,0 +1,72 @@
+export const recommenData = [
+  {
+    id: 1,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1543874768-af0b9c4090d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 2,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 3,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 4,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 5,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 6,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 7,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 8,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 9,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 10,
+    name: "Malioboro",
+    location: "Yogyakarta, DIY",
+    imageURL:
+      "https://images.unsplash.com/photo-1679939099437-5577669eebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+]
